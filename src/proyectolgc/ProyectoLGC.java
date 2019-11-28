03114d27c974f883707e8ec0285b456c2a0b5096
luis_ <luis_@DESKTOP-TLG581O>
2019-11-28 16:47:55 +0100

    iniciamos

---
+++ src/proyectolgc/ProyectoLGC.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectolgc;
+
+/**
+ *
+ * @author luis_
+ */
+public class ProyectoLGC {
+    private static String veredicto(String valor1, String valor2) {
+        if (valor1.equals(valor2)) {
+            return "Perfecto. Has tenido buen criterio.";
+        }
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)) {
+            return "Has mejorado, o te ha llegado el éxito inesperado.";
+        }
+        return "Te has confiado. Falta realismo.";
+    }
+       
+    
+    public static void main(String[] args) {
+        String valor1=args[1];
+        String valor2=args[2];
+        System.out.println("La calificacion de " +args[0]+ ":");
+        System.out.println("La calificacion que creo que tengo es: " +args[1]+ ":");
+        System.out.println("La calificacion que realmente tengo es: " +args[2]+ ":");
+        System.out.println(veredicto(valor1,valor2));
+    }
+    
+}
